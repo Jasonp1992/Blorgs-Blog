54b92cb751ea4c3d73490ccfcae0201722abe7c5
jason <potvinj@gmail.com>
2023-03-13 13:23:44 -0400

    add header to home page

---
+++ client/src/components/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+const Homepage: React.FC = () => {
+    return (
+        <div className="homepage">
+            <Header />
+        </div>
+    )
+}
+
+export default Homepage;

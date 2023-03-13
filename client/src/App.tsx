ca27591a96c74ed6fca34700ebcf6b5fc07ea059
jason <potvinj@gmail.com>
2023-03-13 13:24:45 -0400

    add homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homepage from './components/Homepage';
 
-function App() {
-
+const App: React.FC = () => {
   return (
     <div className="App">
+      <Homepage />
     </div>
   );
 }

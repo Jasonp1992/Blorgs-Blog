ea66ce3db523fdf178e7c580570f9bb44d0ef943
jason <potvinj@gmail.com>
2023-03-13 16:54:56 -0400

    add single blog card for styling and testing

--- client/src/components/Homepage.tsx
+++ client/src/components/Homepage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
+import BlogCard from "./BlogCard";
 
 const Homepage: React.FC = () => {
     return (
         <div className="homepage">
             <Header />
+            <BlogCard />
+            <Footer />
         </div>
     )
 }

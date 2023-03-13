7899d7741a9499e1e89579634a4c2c78312e058f
jason <potvinj@gmail.com>
2023-03-13 12:59:07 -0400

    house cleaning

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState(null)
-
-  useEffect(()=>{
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-  }, [])
 
   return (
     <div className="App">
-      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }

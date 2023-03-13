be85faf71e1e7d6101cd36bddc2d9f5977892487
jason <potvinj@gmail.com>
2023-03-13 12:19:35 -0400

    set up

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(()=>{
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+  }, [])
+
+  return (
+    <div className="App">
+      <p>{!data ? "Loading..." : data}</p>
+    </div>
+  );
+}
+
+export default App;
